7d978632343fe826f1d33af74cc642e3e8e811af
Tilly-py <alex.s.tilly@outlook.com>
2026-05-27 14:08:14 +0200

    bootstrap styling
    Added some bootstrap styling to the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import QuestionCard from './components/QuestionCard.jsx/QuestionCard.jsx';
 import Status from './components/Status/Status.jsx';
+import Container from 'react-bootstrap/Container';
 
 import './App.css';
 
 function App() {
   // const [correct, setCorrect] = useState(0);
   // const [incorrect, setIncorrect] = useState(0);
+  // const handleUserAnswer = (selectedOption) => {
+  //   if(selectedOption.isTrue) {
+  //     // Släng in poäng hantering
+  //   })
+  // }o
+  
 
   return (
-    <div className="App">
-      <h1>Questions 4 Dummies</h1>
-      <Status correct={0} incorrect={0} />
-      <QuestionCard />
-    </div>
+    <Container>
+      <div className="App">
+        <h1 className="mt-2">Questions 4 Dummies</h1>
+        <Status correct={0} incorrect={0} />
+        <QuestionCard />
+      </div>
+    </Container>
   );
 }
 
--- src/api/notFacts.js
+++ src/api/notFacts.js
@@ -4,6 +4,7 @@ const BASE_URL = 'https://notfacts.org/api?query=';
 const getNotFact = async (query) => {
   const response = await fetch(`${BASE_URL}${encodeURIComponent(query)}`);
   if (!response.ok) {
+    console.log(response.statusText);
     throw new Error('Could not fetch not facts');
   }
   const data = await response.json();
